526836e4b752df31127b112e09f126c6623c431b
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2025-12-08 16:44:51 +0530

    feat: add user, address, and payload interfaces for improved type safety

---
+++ types/user.d.ts
@@ -0,0 +1,57 @@
+interface User {
+  id: number;
+  name: string;
+  phone: string;
+  email?: string;
+  role: "user" | "admin" | "delivery";
+  image?: string;
+  addresses?: Address[];
+}
+
+interface Address {
+  id: number | string;
+  name: string;
+  phone: string;
+  line1: string;
+  line2?: string;
+  street?: string;
+  country: string;
+  state: string;
+  city: string;
+  pinCode: string | number;
+  geoLocation?: string;
+}
+interface NewAddressPayload {
+  name: string;
+  phone: string;
+  line1: string;
+  line2?: string;
+  street?: string;
+  country: string;
+  state: string;
+  city: string;
+  pinCode: string | number;
+  geoLocation?: string;
+}
+
+interface RegisterPayload {
+  name: string;
+  phone: string;
+  requestId?: string;
+  deviceInfo?: string;
+}
+
+interface VerifyOtpPayload {
+  phone: string;
+  requestId: string;
+  otp: string;
+  deviceInfo: string;
+}
+
+export type {
+  User,
+  Address,
+  RegisterPayload,
+  VerifyOtpPayload,
+  NewAddressPayload,
+};
